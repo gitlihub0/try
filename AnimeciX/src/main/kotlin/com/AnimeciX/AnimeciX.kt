4b2b6f4b2c1d0b5633c413702fc8774620ff6c93
gitlihub0 <git0hub00@proton.me>
2026-08-19 14:49:43 +0300

    Update AnimeciX.kt

--- AnimeciX/src/main/kotlin/com/AnimeciX/AnimeciX.kt
+++ AnimeciX/src/main/kotlin/com/AnimeciX/AnimeciX.kt
@@ -104,7 +104,7 @@ class AnimeciX : MainAPI() {
             this.plot      = response.title.description
             this.episodes = mutableMapOf(DubStatus.Subbed to episodes)
             this.tags      = response.title.tags.map { it.name }
-            this.rating    = response.title.rating.toRatingInt()
+            this.score = Score.from10(response.title.rating)
             addActors(response.title.actors.map { Actor(it.name, fixUrlNull(it.poster)) })
             addTrailer(response.title.trailer)
         }
